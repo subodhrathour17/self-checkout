5b05e7f5b4ae45188656442dfc24a7990bc3d0ef
devgauravsuntech <devgaurav900@gmail.com>
2022-06-01 11:09:59 +0530

    latest commit for the code

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import Logo from "./suntech-logo.png";
+
+// Login Page Images
+import BflGroupLogo from './bfl-group-logo.png'
+import Merge from './merge.png'
+import Vector1 from './vector1.png'
+import Vector2 from './vector2.png'
+import Vector6 from './vector6.png'
+
+import Rectangle30 from './rectangle30.png';
+import Young from './young.png';
+import Comp1 from './comp1.png';
+import Girl from './girl.png';
+
+export { Logo, BflGroupLogo, Merge, Vector2, Vector6, Vector1, Rectangle30, Young, Comp1, Girl };
